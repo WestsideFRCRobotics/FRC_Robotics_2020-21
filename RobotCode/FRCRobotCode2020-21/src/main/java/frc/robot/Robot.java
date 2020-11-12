4f15de1983fa509015e90ccf71bdb41f73016e8a
ClaytonSmith314 <44728116+ClaytonSmith314@users.noreply.github.com>
2020-11-12 17:31:54 -0600

    Update Robot.java

--- RobotCode/FRCRobotCode2020-21/src/main/java/frc/robot/Robot.java
+++ RobotCode/FRCRobotCode2020-21/src/main/java/frc/robot/Robot.java
@@ -11,6 +11,9 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+//Robot Code for Westside FRC for the 2020-2021 competition
+
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
