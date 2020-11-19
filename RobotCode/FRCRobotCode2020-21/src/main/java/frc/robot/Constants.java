3a02ee718bcc34a3cb1786943f350a3f06d5f6ba
ClaytonSmith314 <44728116+ClaytonSmith314@users.noreply.github.com>
2020-11-19 17:20:53 -0600

    added can id for right drive motor

--- RobotCode/FRCRobotCode2020-21/src/main/java/frc/robot/Constants.java
+++ RobotCode/FRCRobotCode2020-21/src/main/java/frc/robot/Constants.java
@@ -16,4 +16,7 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+    public static final int RIGHT_DRIVE_MOTOR_FRONT = 0; //TODO: get CAN bus value
+
 }
