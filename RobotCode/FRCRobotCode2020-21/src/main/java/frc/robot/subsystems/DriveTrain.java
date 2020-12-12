16c9678c66006a439205a2748dbd8bb7c08d4a31
AdrienChew246 <s1736607@online.houstonisd.org>
2020-12-12 13:03:52 -0600

    Finished Drive Train

--- RobotCode/FRCRobotCode2020-21/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ RobotCode/FRCRobotCode2020-21/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -41,8 +41,30 @@ public class DriveTrain extends SubsystemBase {
    */
   public DriveTrain() {
     //TODO: Add initialisers and stuff like methods
-    
+    LeftMotor1.setInverted(true);
+    LeftMotor2.setInverted(true);
+
+    LeftMotor2.follow(LEFT);
+    RightMotor2.follow(RIGHT);
+
   }
+
+  //method for setting speed of left motor
+  public void setLeftMotors(double percent) {
+    LEFT.set(ControlMode.PercentOutput, percent);
+  }
+
+  //method for setting speed for right motor
+  public void setRightMotors(double percent) {
+    RIGHT.set(ControlMode.PercentOutput, percent);
+  }
+
+  public void drive(double speed, double turnH) {
+    setRightMotors(speed - turnH);
+    setLeftMotors(speed + turnH);
+  }
+
+  
 
   @Override
   public void periodic() {
