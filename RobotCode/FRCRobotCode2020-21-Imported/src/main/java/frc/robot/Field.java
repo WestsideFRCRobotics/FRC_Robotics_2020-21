a5a46b756cff04747d09674a422f2b55f7e488aa
hobob123 <73672291+hobob123@users.noreply.github.com>
2021-01-28 15:01:34 -0600

    Added more features to the feild class
    Added more comment. More needed :D

--- RobotCode/FRCRobotCode2020-21-Imported/src/main/java/frc/robot/Field.java
+++ RobotCode/FRCRobotCode2020-21-Imported/src/main/java/frc/robot/Field.java
@@ -2,7 +2,12 @@ package frc.robot;
 
 public class Field 
 {
-    private String[][] field_cords= {
+    //Robot position variable x pos and then y pos
+    public double[] robotpos = {0.0, 0.0};
+
+    public double robot_angle = 0;
+
+    public String[][] field_cords= {
                                     {"Z0", "Z1", "Z2", "Z3", "Z4", "Z5", "Z6", "Z7", "Z8", "Z9", "Z10", "Z11", "Z12"},
                                     {"A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12"},
                                     {"B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12"},
@@ -12,8 +17,15 @@ public class Field
                                     {"F0", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12"},
                                    }; // A LIST OF ALL POSSIBLE ROBOT AND ITEM LOCATIONS. AcTuAl BrAin DeAtH
     
-    public double[] Cord_Pos_Ft (char y, int x)
+    public double[] Cord_Pos_Ft (String cord)
     {
+        
+        //Input: A string cordinate. The letter must be uppercase.
+        //Output: a list of two doubles, the x position and y position
+        
+
+        char y = cord.charAt(0);
+        int x = (int) cord.charAt(1);
         int y_counter = 0; // establishes a counter to count the number of loops for y
         int x_counter = 0; // establishes a counter to count the number of loops for x
         double y_final = 0.0; // returned y val
@@ -55,5 +67,55 @@ public class Field
         return returnlist;
     }
 
+    public void Update_Robot_Pos (double[] newpos)
+    {
+        
+        //Input: double list of robot position
+        //Output: None
+        
+        robotpos = newpos;
+    }
+
+    public double[] Get_Distance_From_Point (String cord)
+    {
+        
+        //Input: The point you want to get a distance to (A string)
+        //Output: The distance in the x axis and the distance in the y axis in feet.
+
+        //If x axis is negative, then the poit is to the left of the robot on the x axis
+        //IF y axis is negative, then the point is to the right of the robot on the y axis
+        
+
+        double[] Robot_Cords = robotpos;
+
+        double[] Point_Feet_Cords = Cord_Pos_Ft(cord);
+
+        double Delta_X = Point_Feet_Cords[0] - Robot_Cords[0];
+
+        double Delta_Y = Point_Feet_Cords[1] - Robot_Cords[1];
+
+        double[] returnlis = {Delta_X, Delta_Y};
+
+        return returnlis;
+    }
+
+
+    public double[] Get_Optimum_angle_To_Point_and_Distance (String cord)
+    {
+        // Input - cordinate
+        // Output - List with angle and distance needed top drive to reach the point.
+
+        double[] deltalist = Get_Distance_From_Point(cord);
+
+        double angle_in_rad = Math.atan(deltalist[1]/deltalist[0]); 
+
+        double angle = Math.toDegrees(angle_in_rad);
+
+        double hypotenuse = Math.sqrt(deltalist[0]*deltalist[0] + deltalist[1]*deltalist[1]);
+        
+        double[] returnlis = {angle, hypotenuse};
+
+        return returnlis;
+    }
 
 }
