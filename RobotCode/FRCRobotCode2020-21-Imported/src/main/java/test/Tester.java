0da7759dbd334f416490888a4474a3d3724b7d83
ClaytonSmith314 <44728116+ClaytonSmith314@users.noreply.github.com>
2021-01-29 10:19:24 -0600

    Added a tester class to test some of the new code we are building

---
+++ RobotCode/FRCRobotCode2020-21-Imported/src/main/java/test/Tester.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Arrays;
+
+import frc.robot.Field;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        Field field = new Field();
+
+        System.out.println(Arrays.toString(field.Cord_Pos_Ft("B3")));
+
+    }
+    
+}
