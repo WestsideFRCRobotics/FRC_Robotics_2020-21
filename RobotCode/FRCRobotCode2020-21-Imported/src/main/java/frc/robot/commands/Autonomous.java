9301de9d10518cd6e294e87df2b79779831975e4
ClaytonSmith314 <44728116+ClaytonSmith314@users.noreply.github.com>
2021-03-09 19:14:11 -0600

    Update Autonomous.java

--- RobotCode/FRCRobotCode2020-21-Imported/src/main/java/frc/robot/commands/Autonomous.java
+++ RobotCode/FRCRobotCode2020-21-Imported/src/main/java/frc/robot/commands/Autonomous.java
@@ -1,34 +1,36 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.*;
+import frc.robot.*;
 
 public class Autonomous {
 
     private final DriveTrain driveTrain;
-
-    private final TeleOp teleOp;
-    //Takes input from the XboxController and changes it into output for the DriveTrain
-  
-    private final XboxController controller;
-  
-  
   
     private final Movement movement;
   
     private final Field field;
 
 
+    public Autonomous(DriveTrain driveTrain, Movement movement, Field field) {
+        this.driveTrain = driveTrain;
+        this.movement = movement;
+        this.field = field;
+    }
+
+
 
     public Command Square() {
+
         return new SequentialCommandGroup(
-            new InstantCommand()
+            new InstantCommand(()->movement.resetAngle()),
             new DriveStraight(3, 0, driveTrain, movement),
 
             new TurnToAngle(TargetAngleDegrees, driveTrain, movement)
 
         );
-
-
 
     }
 
