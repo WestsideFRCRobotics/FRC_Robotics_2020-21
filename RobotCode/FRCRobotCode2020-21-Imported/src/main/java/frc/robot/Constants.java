455b99031a1ce884dca742196a83e1a569de09bf
hobob123 <73672291+hobob123@users.noreply.github.com>
2022-11-08 21:03:58 -0600

    HELLO!

--- RobotCode/FRCRobotCode2020-21-Imported/src/main/java/frc/robot/Constants.java
+++ RobotCode/FRCRobotCode2020-21-Imported/src/main/java/frc/robot/Constants.java
@@ -52,7 +52,7 @@ public final class Constants {
 
     public static final double MAX_SPEED_FT_PER_SECOND = -1234;
 
-    //Conversion Constants
+    //Conversion Constants for mag encoders
     public static final double TICKSPER100MS_PER_RPM = 4096.0/600;
     public static final double TICKSPER100MS_PER_FTPERSEC = 4096.0/(8.0/12*Math.PI)*.1; //for 8in diameter wheel
     public static final double DISTANCE_DRIVE_FEET_PER_MAG_TICK = 8.0/12*Math.PI/4096.0;
