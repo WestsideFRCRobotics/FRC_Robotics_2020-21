e99926cb3438c6f0ccf92a30138c5d3a45282dac
AdrienChew246 <s1736607@online.houstonisd.org>
2023-02-25 09:39:40 -0600

    Cleaned up code

--- Vision Test/src/main/java/frc/robot/Robot.java
+++ Vision Test/src/main/java/frc/robot/Robot.java
@@ -1,19 +1,8 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-
-/*
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-*/
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import java.util.List;
@@ -22,60 +11,25 @@ import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends TimedRobot {
-  
-  /*
-  private final PWMSparkMax m_leftDrive = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightDrive = new PWMSparkMax(1);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
-  private final XboxController m_controller = new XboxController(0);
-  private final Timer m_timer = new Timer();
-  */
 
   public static PhotonCamera camera = new PhotonCamera("FHD_Camera");
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
   @Override
   public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
 
     ShuffleboardTab tab = Shuffleboard.getTab("Vision");
     tab.addCamera("View", "FHD_Camera");
   }
 
-  /** This function is run once each time the robot enters autonomous mode. */
   @Override
   public void autonomousInit() {
-    //m_timer.reset();
-    //m_timer.start();
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    // Drive for 2 seconds
-    /*
-    if (m_timer.get() < 2.0) {
-      // Drive forwards half speed, make sure to turn input squaring off
-      m_robotDrive.arcadeDrive(0.5, 0.0, false);
-    } else {
-      m_robotDrive.stopMotor(); // stop robot
-    }
-    */
   }
 
-  /** This function is called once each time the robot enters teleoperated mode. */
   @Override
   public void teleopInit() {}
 
@@ -94,26 +48,13 @@ public class Robot extends TimedRobot {
     }
   }
 
-  /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    //m_robotDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getRightX());
-    /*PhotonPipelineResult cameraResult = camera.getLatestResult();
-    if(cameraResult.hasTargets()){
-      List<PhotonTrackedTarget> targets = cameraResult.getTargets();
-      for(PhotonTrackedTarget target : targets){
-        System.out.println(target.getFiducialId());
-        System.out.println(target.getBestCameraToTarget());
-      }
-    }*/
-    System.out.println("e");
   }
 
-  /** This function is called once each time the robot enters test mode. */
   @Override
   public void testInit() {}
 
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 }
