9d266d85dc0b2b8d5678c1c8b5715448130f8f75
AdrienChew246 <s1736607@online.houstonisd.org>
2023-02-25 15:56:11 -0600

    Started programming vision odometry

--- Vision Test/src/main/java/frc/robot/Constants.java
+++ Vision Test/src/main/java/frc/robot/Constants.java
@@ -1,14 +1,46 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.util.Units;
 
 public class Constants {
+
+    public static final Pose3d robot = new Pose3d(); 
 
     public static final class AprilTag {
 
         public static final double targetWidth = Units.inchesToMeters(100);         //Width of AprilTag
         public static final double targetHeight = Units.inchesToMeters(100);        //Height of AprilTag
 
+        //Camera Constants
+        public static final String cameraName = "OV5647";
+        public static final double cameraMaxDiagonalFOV = 70;
+        public static final Transform3d robotToCamera = new Transform3d();
+        public static final double cameraMaxLEDRange = 9999;
+        public static final int cameraResWidth = 640;
+        public static final int cameraResHeight = 480;
+        public static final double cameraMinTargetArea = 10;
+
+
+
+
+        //AprilTag locations on field
+        //Red alliance community (right to left)
+        public static final Pose3d tag1 = new Pose3d(Units.inchesToMeters(610.77), Units.inchesToMeters(42.19), Units.inchesToMeters(18.22), new Rotation3d(0, 0, Math.PI));
+        public static final Pose3d tag2 = new Pose3d(Units.inchesToMeters(610.77), Units.inchesToMeters(108.19), Units.inchesToMeters(18.22), new Rotation3d(0, 0, Math.PI));
+        public static final Pose3d tag3 = new Pose3d(Units.inchesToMeters(610.77), Units.inchesToMeters(174.19), Units.inchesToMeters(18.22), new Rotation3d(0, 0, Math.PI));
+
+        //Blue alliance substation
+        public static final Pose3d tag4 = new Pose3d(Units.inchesToMeters(636.96), Units.inchesToMeters(265.74), Units.inchesToMeters(27.38), new Rotation3d(0, 0, Math.PI));
+
+        //Red alliance substation
+        public static final Pose3d tag5 = new Pose3d(Units.inchesToMeters(14.25), Units.inchesToMeters(265.74), Units.inchesToMeters(27.38), new Rotation3d(0, 0, Math.PI));
         
+        //Blue alliance community (right to left)
+        public static final Pose3d tag6 = new Pose3d(Units.inchesToMeters(40.45), Units.inchesToMeters(174.19), Units.inchesToMeters(18.22), new Rotation3d(0, 0, Math.PI));
+        public static final Pose3d tag7 = new Pose3d(Units.inchesToMeters(40.45), Units.inchesToMeters(108.19), Units.inchesToMeters(18.22), new Rotation3d(0, 0, Math.PI));
+        public static final Pose3d tag8 = new Pose3d(Units.inchesToMeters(40.45), Units.inchesToMeters(42.19), Units.inchesToMeters(18.22), new Rotation3d(0, 0, Math.PI));
     }
 }
--- Vision Test/src/main/java/frc/robot/Robot.java
+++ Vision Test/src/main/java/frc/robot/Robot.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -8,17 +11,21 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import java.util.List;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
+import org.photonvision.SimPhotonCamera;
+import org.photonvision.SimVisionSystem;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class Robot extends TimedRobot {
 
-  public static PhotonCamera camera = new PhotonCamera("FHD_Camera");
+  public static PhotonCamera camera = new PhotonCamera(Constants.AprilTag.cameraName); //OV5647     FHD_Camera
+  public static SimPhotonCamera simCamera = new SimPhotonCamera(Constants.AprilTag.cameraName);
 
   @Override
   public void robotInit() {
+
     ShuffleboardTab tab = Shuffleboard.getTab("Vision");
-    tab.addCamera("View", "FHD_Camera");
+    //tab.addCamera("View", "OV5647");
   }
 
   @Override
@@ -34,15 +41,22 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotPeriodic() {
+    SimVisionSystem simVision = new SimVisionSystem(Constants.AprilTag.cameraName, Constants.AprilTag.cameraMaxDiagonalFOV, Constants.AprilTag.robotToCamera, Constants.AprilTag.cameraMaxLEDRange, Constants.AprilTag.cameraResWidth, Constants.AprilTag.cameraResHeight, Constants.AprilTag.cameraMinTargetArea);
+
     PhotonPipelineResult cameraResult = camera.getLatestResult();
     if(cameraResult.hasTargets()){
       List<PhotonTrackedTarget> targets = cameraResult.getTargets();
       int cnt = 0;
       for(PhotonTrackedTarget target : targets){
+      //simVision.addSimVisionTarget(Constants.AprilTag.);
+        simCamera.submitProcessedFrame(cnt, targets);
+        SmartDashboard.putString("name", ("tag" + target.getFiducialId()));
         SmartDashboard.putNumber("id" + String.valueOf(cnt), target.getFiducialId());
         SmartDashboard.putNumberArray("coordinates "+ String.valueOf(target.getFiducialId()), new double[]
         {target.getBestCameraToTarget().getX(),target.getBestCameraToTarget().getY(),target.getBestCameraToTarget().getZ()});
         SmartDashboard.putNumber("distance to target", PhotonUtils.calculateDistanceToTargetMeters(kDefaultPeriod, kDefaultPeriod, kDefaultPeriod, kDefaultPeriod));
+        SmartDashboard.putNumber( "Area", target.getArea());
+
 
         
 
